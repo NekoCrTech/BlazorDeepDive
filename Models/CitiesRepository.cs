b28e0bbf8317d34d4f053c2bd6aae8f245afa171
nektpol <nekocreativetechnologies@gmail.com>
2025-09-20 12:40:10 +0300

    Initialize blazor project

---
+++ Models/CitiesRepository.cs
@@ -0,0 +1,15 @@
+namespace serverManagement2.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+        public static List<string> GetCities() => cities;
+    }
+}
