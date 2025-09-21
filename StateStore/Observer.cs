abfee06fecffd45c71c3d97a54e5a4cfed092fef
nektpol <nekocreativetechnologies@gmail.com>
2025-09-22 01:27:50 +0300

    Implement Observer pattern

---
+++ StateStore/Observer.cs
@@ -0,0 +1,24 @@
+﻿namespace serverManagement2.StateStore
+{
+    public class Observer
+    {
+        protected Action? _listeners;
+
+        public void AddStateChangeListeners(Action? listener)
+        {
+            if(listener != null) 
+                _listeners += listener;
+        }
+
+        public void RemoveStateChangeListeners(Action? listener) 
+        {
+            if(_listeners != null)
+                _listeners -= listener;
+        }
+
+        public void BroadcastStateChange()
+        {
+            _listeners?.Invoke();
+        }
+    }
+}
---
+++ StateStore/TorontoOnlineServersStore.cs
@@ -0,0 +1,18 @@
+﻿namespace serverManagement2.StateStore
+{
+    public class TorontoOnlineServersStore : Observer
+    {
+        private int _numServersOnline;
+
+        public int GetNumberServersOnline() 
+        {  
+            return _numServersOnline; 
+        }
+
+        public void SetNumberServersOnline(int number) 
+        {
+            _numServersOnline = number;
+            base.BroadcastStateChange();
+        }
+    }
+}
